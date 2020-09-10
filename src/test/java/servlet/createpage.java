b9ddc7e19ba1d9f3bddf27039d3144930fcf5ecc
prudhvi1718 <70396244+prudhvi1718@users.noreply.github.com>
2020-09-10 00:49:08 -0400

    Update createpage.java
    Updated QA IP address

--- src/test/java/servlet/createpage.java
+++ src/test/java/servlet/createpage.java
@@ -17,7 +17,7 @@ public class createpage extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://34.75.27.101:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://35.196.240.133:5432/postgres";
 			String username="postgres";
 			String password="password";
 		
